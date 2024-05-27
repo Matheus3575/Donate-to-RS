7bca797a97e78874fa46c8a0c3a6ac88f9401696
Matheus Coelho <43256956+Matheus3575@users.noreply.github.com>
2024-05-27 09:53:30 -0300

    Final Version

--- aulaFinal/script.js
+++ aulaFinal/script.js
@@ -1,25 +1,33 @@
 document.addEventListener("scroll", function() {
     var scrollTop = document.documentElement.scrollTop;
-    var scrollHeight = document.documentElement.scrollHeight - window.innerHeightHeight;
+    var scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
     var scrollPercentage = scrollTop / scrollHeight;
 
     var waterHeight = (1 - scrollPercentage) * 100;
     var waterSides = document.querySelectorAll(".water-side");
 
     waterSides.forEach(function(side) {
-        side.style.height = waterHeight - "%";
+        side.style.height = waterHeight + "%";
     });
 });
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show')
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
         }
-
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
